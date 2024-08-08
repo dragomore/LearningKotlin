e86f35b00fa4e74b23d049b95d900843d3e59ce1
dragomore <dragomore2804@gmail.com>
2024-08-08 19:57:02 +0300

    Upload new task

---
+++ src/test/kotlin/ShortestWordTest.kt
@@ -0,0 +1,17 @@
+import org.example.ShortestWord
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class ShortestWordTest {
+
+    @Test
+    fun findShort() {
+        val foo = ShortestWord()
+        val findShort = foo::findShort
+
+        assertEquals( 3, findShort( "String about nature two" ) )
+        assertEquals( 1, findShort( "Today i wash my cloth" ) )
+        assertEquals( 9, findShort( "Wednesday Wednesdayyyyy" ) )
+    }
+}
