e9d9db420e0f7b7a20bbbc16afe7493f3b7c6200
dragomore <dragomore2804@gmail.com>
2024-08-09 20:15:32 +0300

    Fix dry

--- src/main/kotlin/GetMiddleCharacter.kt
+++ src/main/kotlin/GetMiddleCharacter.kt
@@ -2,12 +2,13 @@ package org.example
 
 class GetMiddleCharacter {
     fun getMiddle( word: String ): String{
-        val wordLength = word.length
         if( word.isEmpty() )
             return ""
-        else if( word.length % 2 == 0 )
-            return "${ word[ wordLength / 2 - 1 ] }${ word[ wordLength / 2 ]}"
-        else
-            return word[wordLength / 2].toString()
+        val wordLength = word.length
+        val middleCharIdx = wordLength / 2
+        return if( word.length % 2 == 0 ) {
+            "${word[middleCharIdx - 1]}${word[middleCharIdx / 2]}"
+        } else
+            word[wordLength / 2].toString()
     }
 }
