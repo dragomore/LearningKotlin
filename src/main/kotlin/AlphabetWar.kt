5baf03acd782f8cf05923789cf08a960ece03af1
dragomore <dragomore2804@gmail.com>
2024-08-13 20:56:59 +0300

    Add function for calculate battle score

--- src/main/kotlin/AlphabetWar.kt
+++ src/main/kotlin/AlphabetWar.kt
@@ -14,19 +14,21 @@ class AlphabetWar {
             'd' to 2,
             'z' to 1
         )
-
         val leftSideWins = "Left side wins!"
         val rightSideWins = "Right side wins!"
         val draw = "Let's fight again!"
-
-        val leftSidePower = fight.filter { it in leftSide }.sumOf{ leftSide[it] ?: 0 }
-        val rightSidePower = fight.filter{ it in rightSide }.sumOf{ rightSide[it] ?: 0}
-        val battleResult = leftSidePower - rightSidePower
+        val battleResult = getBattleResult( fight, leftSide, rightSide )
 
         if( battleResult == 0 )
             return draw
         if( battleResult > 0 )
             return leftSideWins
         return rightSideWins
+    }
+
+    private fun getBattleResult( str: String, leftScore: Map<Char, Int>, rightScore: Map<Char, Int> ): Int{
+        val leftSidePower = str.filter { it in leftScore }.sumOf{ leftScore[it] ?: 0 }
+        val rightSidePower = str.filter{ it in rightScore }.sumOf{ rightScore[it] ?: 0 }
+        return leftSidePower - rightSidePower
     }
 }
