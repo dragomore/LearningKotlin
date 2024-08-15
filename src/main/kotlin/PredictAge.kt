68b9e9b6f3d32a0bb2b01742cf2d35c0c4673bca
dragomore <dragomore2804@gmail.com>
2024-08-16 01:13:11 +0300

    Upload new task

---
+++ src/main/kotlin/PredictAge.kt
@@ -0,0 +1,13 @@
+package org.example
+
+import kotlin.math.sqrt
+
+class PredictAge {
+    fun predictAge( ages: List<Int> ): Int{
+        val multiplyAges = ages.map{ it * it }
+        val sumMultiplies = multiplyAges.sumOf{ it }
+        val squareResultOfSum = sqrt( sumMultiplies.toDouble() ).toInt()
+
+        return squareResultOfSum / 2
+    }
+}
