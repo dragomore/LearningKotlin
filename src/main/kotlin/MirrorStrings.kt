e532ef8cc213758ab813b610d3022f6d5c82fa3b
dragomore <dragomore2804@gmail.com>
2024-08-17 17:31:40 +0300

    Upload new task

---
+++ src/main/kotlin/MirrorStrings.kt
@@ -0,0 +1,21 @@
+package org.example
+
+class MirrorStrings {
+    fun oper( function: ( String ) -> String , s: String): String = function( s )
+
+    fun vertMirror( str: String ): String {
+        return str.split("\n" )
+            .map{ it.reversed() }
+            .joinToString { it }
+            .replace(" ", "" )
+            .replace(",", "\n" )
+    }
+    fun horMirror( str: String ): String {
+        return str.split("\n" )
+            .map{ it }
+            .reversed()
+            .joinToString { it }
+            .replace(" ","" )
+            .replace(",", "\n" )
+    }
+}
