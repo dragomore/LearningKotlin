f7df58e646c11a49c7f5182da1758936b8a0fa53
dragomore <dragomore2804@gmail.com>
2024-08-18 16:31:25 +0300

    Add function which replace characters

--- src/main/kotlin/AltCap.kt
+++ src/main/kotlin/AltCap.kt
@@ -1,22 +1,29 @@
 package org.example
 
 class AltCap {
-    fun capitalize( text: String ): List<String> {
-        val capitalizeEven = StringBuilder()
-        text.mapIndexed { idx, char ->
-            if( idx % 2 == 0 )
-                capitalizeEven.append( char.uppercase() )
-            else
-                capitalizeEven.append( char )
-        }
-        val capitalizeOdd = StringBuilder()
-        text.mapIndexed { idx, char ->
-            if( idx % 2 != 0 )
-                capitalizeOdd.append( char.uppercase() )
-            else
-                capitalizeOdd.append( char )
+    fun capitalize( text: String ): List<String> =  listOf( capitalizeLetters( text, "even") , capitalizeLetters( text, "odd") )
+
+    private fun capitalizeLetters( str: String, method: String ): String{
+        val capitalizedString = StringBuilder()
+        when( method ){
+            "even" -> {
+                str.mapIndexed { idx, char ->
+                    if( idx % 2 == 0 )
+                        capitalizedString.append( char.uppercase() )
+                    else
+                        capitalizedString.append( char )
+                }
+            }
+            "odd" -> {
+                str.mapIndexed { idx, char ->
+                    if( idx % 2 != 0 )
+                        capitalizedString.append( char.uppercase() )
+                    else
+                        capitalizedString.append( char )
+                }
+            }
         }
 
-        return listOf( capitalizeEven.toString() , capitalizeOdd.toString() )
+        return capitalizedString.toString()
     }
 }
