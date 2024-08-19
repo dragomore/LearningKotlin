2ebd22d81b430d7acf88d70bef5a46a4fb82a521
dragomore <dragomore2804@gmail.com>
2024-08-19 19:32:26 +0300

    Upload new task

---
+++ src/test/kotlin/ScreenTest.kt
@@ -0,0 +1,17 @@
+import org.example.Screen
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class ScreenTest {
+
+    @Test
+    fun findScreenHeight() {
+        val foo = Screen()
+        val findScreenHeight = foo::findScreenHeight
+
+        assertEquals("1024x768", findScreenHeight(1024,"4:3"))
+        assertEquals("1280x720", findScreenHeight(1280,"16:9"))
+        assertEquals("3840x1080", findScreenHeight(3840,"32:9"))
+    }
+}
